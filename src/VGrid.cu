a43ad87e0c1e6084e9010782b987f4913467b681
jaycedowell <jdowell@unm.edu>
2021-12-17 16:03:12 -0700

    Apply suggestions from code review

--- src/VGrid.cu
+++ src/VGrid.cu
@@ -287,8 +287,8 @@ public:
     void set_kernels(BFarray const* kernels) {
         BF_TRACE();
         BF_TRACE_STREAM(_stream);
-        BF_ASSERT_EXCEPTION(kernels->dtype == BF_DTYPE_CF32 \
-                                              || BF_DTYPE_CF64, BF_STATUS_UNSUPPORTED_DTYPE);
+        BF_ASSERT_EXCEPTION((kernels->dtype == BF_DTYPE_CF32) \
+                                              || (kernels->dtype == BF_DTYPE_CF64), BF_STATUS_UNSUPPORTED_DTYPE);
         
         int nkernels = kernels->shape[0];
         for(int i=1; i<kernels->ndim-4; ++i) {
@@ -306,8 +306,8 @@ public:
 	BF_ASSERT_EXCEPTION(_y != NULL, BF_STATUS_INVALID_STATE);
 	BF_ASSERT_EXCEPTION(_z != NULL, BF_STATUS_INVALID_STATE);
         BF_ASSERT_EXCEPTION(_kernels != NULL, BF_STATUS_INVALID_STATE);
-        BF_ASSERT_EXCEPTION(out->dtype == BF_DTYPE_CF32 \
-                                          || BF_DTYPE_CF64, BF_STATUS_UNSUPPORTED_DTYPE);
+        BF_ASSERT_EXCEPTION((out->dtype == BF_DTYPE_CF32) \
+                                          || (out->dtype == BF_DTYPE_CF64), BF_STATUS_UNSUPPORTED_DTYPE);
         
         BF_CHECK_CUDA_EXCEPTION(cudaGetLastError(), BF_STATUS_INTERNAL_ERROR);
         
